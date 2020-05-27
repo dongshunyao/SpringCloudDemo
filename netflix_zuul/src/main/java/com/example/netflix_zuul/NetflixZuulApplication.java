3cdd2413303a7ce55f258d46e16f2970fe0926b2
董舜尧 <dongshunyao@qq.com>
2020-05-27 20:19:52 +0800

    创建 spring cloud netflix zuul 模块

---
+++ netflix_zuul/src/main/java/com/example/netflix_zuul/NetflixZuulApplication.java
@@ -0,0 +1,17 @@
+package com.example.netflix_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class NetflixZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZuulApplication.class, args);
+    }
+
+}
