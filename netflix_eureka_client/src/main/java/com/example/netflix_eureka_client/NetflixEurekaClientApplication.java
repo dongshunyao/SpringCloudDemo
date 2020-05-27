8b6fc7b205edd2489211f6f5b54dd31cff8fbd6b
董舜尧 <dongshunyao@qq.com>
2020-05-27 18:49:49 +0800

    创建 spring cloud netflix eureka client 模块

---
+++ netflix_eureka_client/src/main/java/com/example/netflix_eureka_client/NetflixEurekaClientApplication.java
@@ -0,0 +1,15 @@
+package com.example.netflix_eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class NetflixEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaClientApplication.class, args);
+    }
+
+}
