8bb580ebde50dcf9eedab1c8a3ab176b1ca000a3
董舜尧 <dongshunyao@qq.com>
2020-06-01 23:01:24 +0800

    使用 RestTemplate 开启负载均衡

--- consumer_demo/src/main/java/com/example/consumer_demo/controller/ConsumerController.java
+++ consumer_demo/src/main/java/com/example/consumer_demo/controller/ConsumerController.java
@@ -1,15 +1,17 @@
 package com.example.consumer_demo.controller;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
+    @LoadBalanced
     private RestTemplate restTemplate = new RestTemplate();
 
     @RequestMapping("/api/v1/demo/get")
     public String consumer() {
-        return "Consumer get message: " + restTemplate.getForObject("http://localhost:8077/producer/produce", String.class);
+        return "Consumer get message: " + restTemplate.getForObject("http://producer_demo/producer/produce", String.class);
     }
 }
